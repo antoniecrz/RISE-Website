06a75f5dfd2abb0c0fa505a1f4476b52098a306d
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-09-30 11:04:57 +0000

    Initial commit

---
+++ src/pages/masterPage.js
@@ -0,0 +1,8 @@
+// The code in this file will load on every page of your site
+$w.onReady(() => {
+
+    let currentYear = new Date().getFullYear();
+
+    $w("#copyrightText").text = `© ${currentYear} RISE Enterprise. All Rights Reserved.`;
+
+});
